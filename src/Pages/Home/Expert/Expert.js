0fd84470ed19f13267e0c7d6df7b9cdedaa141a5
ReNayeem <renayeem@gmail.com>
2022-05-07 01:11:40 +0600

    footer rough completed

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -9,7 +9,7 @@ const Expert = ({ expert }) => {
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className='text-center mt-5'>
-            <p><small>copyright @ {year} </small></p>
+        <footer id="footer">
+            <div className="footer pt-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 text-center">
+                            <div className="footer-social pb-4 text-center">
+                                <h4 className='text-white mb-3'>Stay in touch</h4>
+                                <a href=""><i className="fab fa-facebook"></i></a>
+                                <a href=""><i className="fab fa-twitch"></i></a>
+                                <a href=""><i className="fab fa-youtube"></i></a>
+                                <a href=""><i className="fab fa-dribbble"></i></a>
+                                <a href=""><i className="fab fa-linkedin"></i></a>
+                                <a href=""><i className="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <form className="newsletter">
+                                <div className="d-flex">
+                                    <input className="form-control" placeholder="email address here" type="email" />
+                                    <button className="footer-button" type="submit">Subscribe</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="col-sm-12">
+                            <div className="footer-copy">
+                                <div className="copy-right text-center pt-5">
+                                    <p className="text-light">&copy; {year}. HARMIC. All rights reserved.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
